bb07344c5b65cb053216b7aef0c26ec51d1417d0
Luis Klembt <luis.klembt@gmx.de>
2024-11-08 14:40:12 +0100

    added loading animation with temporary delay, later replaced by database get

--- src/pages/startPage.jsx
+++ src/pages/startPage.jsx
@@ -5,13 +5,39 @@ import { useNavigate } from "react-router-dom";
 
 function StartPage() {
     const [gameID, setGameID] = useState("")
+    const [alert, setAlert] = useState({
+      style: { display: 'none' }, 
+      serching: { display: 'none' },
+      notfound: { display: 'none' },
+      error: { display: 'none' }
+    })
     // const result = useLoaderData()
     const navigate = useNavigate()
-    const enterGame = () => {
+    const delay = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
+    const enterGame = async () => {
       if(gameID){
+        setAlert({
+          style: {  }, 
+          searching: {  },
+          notfound: { display: 'none' },
+          error: { display: 'none' }
+        })
+        await delay(3000)
         navigate('/game/' + gameID)
       }else{
-        alert("Gib etwas ein, um einem Spiel beizutreten.")
+        setAlert({
+          style: {  }, 
+          searching: { display: 'none' },
+          notfound: {  },
+          error: { display: 'none' }
+        })
+        await delay(3000)
+        setAlert({
+          style: { display: 'none' }, 
+          searching: { display: 'none' },
+          notfound: {  },
+          error: { display: 'none' }
+        })
       }
     }
     return (
@@ -80,6 +106,17 @@ function StartPage() {
           <div className="new-quiz">Erstelle dein eigenes Quiz!</div>
         </div>
       </div>
+
+      {/* Alert wenn Spiel ID eingegeben */}
+      <div className='alert' style={alert.style}>
+          <div className='search'>
+            <div className='search-text' style={alert.searching}>Spiel wird gesucht...</div> {/* Wenn gesucht */}
+            <div className='error-search'style={alert.notfound}>Es wurde kein Spiel gefunden. <br></br>Bitte überprüfe deine Quiz-ID. </div> {/* Wenn nix gefunden */}
+            <div className="loader" style={alert.searching}></div> {/* Wenn gesucht */}
+            <div className='try-again' style={alert.error}>Erneut eingegeben</div>{/* Wenn nix gefunden */}
+          </div>
+      </div>
+          {/* Wenn alter geöffnet, soll Hintergrund über normalen Hintergrund grau werden. color= #24242496  */}
     </div>
     );
   }

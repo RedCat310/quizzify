27fea0726b2d90f9c89406bed7ae0066d7d0da25
Its-Maeae <mariusschmi1711@gmail.com>
2024-11-09 16:25:05 +0100

    App Bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import '../styles/AppBar.scss'
+import '../styles/general.scss'
+import logo from '../assets/no_back_logo.png'
+import user from '../assets/user.svg'
 
 function AppBar() {
     return ( <div>
-        Hallo 2.0
+        <div className="appBar">
+        <img className="logo" src={logo}></img>
+            <div className="tags">
+                <div className="tag-1 tag tag-active"><i class="fa-solid fa-house"></i></div>
+                <div className="tag-2 tag"><i class="fa-solid fa-pen"></i></div>
+                <div className="tag-3 tag"><i class="fa-solid fa-cloud-arrow-up"></i></div>
+            </div>
+            <img src={user} alt="" className="user"></img>
+        </div>
     </div> );
 }
 

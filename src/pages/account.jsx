68512e2605a49ffd8bb0f947a452bbbc3a6bb6b7
Its-Maeae <mariusschmi1711@gmail.com>
2024-11-15 20:37:29 +0100

    style quiz übersicht

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -2,6 +2,7 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase";
+import '../styles/account.scss'
 
 function Account() {
     const navigate = useNavigate()
@@ -14,8 +15,52 @@ function Account() {
     })
     return ( 
         <div>
-            Account Page <br></br>
-            <button onClick={() => signOut(auth)}>Logout</button>
+            {/* <button onClick={() => signOut(auth)}>Logout</button> */}
+            <div className="menu-bar">
+                <div className="new-quiz">Erstellen</div>
+            </div>
+            <div className="projekt-view">
+                <div className="projekt">
+                    <i class="fa-solid fa-trash"></i>
+                    <div className="circles">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                    <div className="projekt-name">Test 1</div>
+                    <div className="create-date">15.11.2024</div>
+                </div>
+                <div className="projekt">
+                    <i class="fa-solid fa-trash"></i>
+                    <div className="circles">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                    <div className="projekt-name">Test 2</div>
+                    <div className="create-date">15.11.2024</div>
+                </div>
+
+                <div className="add-projekt"><i class="fa-solid fa-plus"></i></div>
+            </div>
         </div>
      );
 }
